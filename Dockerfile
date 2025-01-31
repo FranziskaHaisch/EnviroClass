6d098576430b440da7a35b058bd4e3fd1f0724d1
Therese Hempel <theresehempel@web.de>
2025-01-31 12:23:00 +0100

    API SetUp

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,22 @@
 # Step 1: Selecting base image with Python + pip installed
 FROM python:3.10.6-buster
 
-# Step 2: Copying project files into container
+# Step 2: Setting working directory within container
+WORKDIR /app
+
+# Step 3: Copying project files into container
 COPY enviro_class /app/enviro_class
 COPY api /app/api
+COPY models /app/models
 COPY requirements.txt /app/
 
-# Step 3: Installing dependencies
+# Step 4: Installing dependencies
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# Step 4: Launching web server with Uvicorn
-CMD uvicorn enviroclass.api.fast:app --host 0.0.0.0 --port $PORT
+# Step 5: Exposing both APIs (wildfire + environment)
+EXPOSE 8001 8002
+
+# STEP 6: Launching web server with Uvicorn (starting both APIs)
+CMD uvicorn api.wildfire:app --host 0.0.0.0 --port 8001 & \
+    uvicorn api.environment:app --host 0.0.0.0 --port 8002
--- enviro_class/modeling.py
+++ enviro_class/modeling.py
@@ -1,6 +1,32 @@
 import tensorflow as tf
+import os
 
-def load_model():
-    """Loading our pre-trained model."""
-    model = tf.keras.models.load_model("enviro_class/model")  # We need to update the path !!!!
-    return model
+
+model = tf.keras.models.load_model("models/test_model.keras")
+model.save("models/test_model.keras", include_optimizer=False)
+
+
+def load_wildfire_model():
+    """Loading WILDFIRE model from CORRECT location"""
+
+    # absolute path
+    absolute_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Moves up one level
+
+    # path to wildfire model
+    wildfire_model_path = os.path.join(absolute_path, "models", "cnn_2classes_ver1.keras")
+
+    # Loading +returning model
+    return tf.keras.models.load_model(wildfire_model_path)
+
+
+# Loading ENVIRONMENT CLASSIFICATION model
+def load_environment_model():
+    """Loading ENVIRONMENT model from CORRECT location"""
+    # absolute path
+    absolute_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Moves up one level
+
+    # path to environment model
+    environment_model_path = os.path.join(absolute_path, "models", "test_model.keras")
+
+    # Loading +returning model
+    return tf.keras.models.load_model(environment_model_path)
